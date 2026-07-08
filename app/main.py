73adc92f7e643421bd2d780baacc5575285a33f8
Abhishek Kumar <abhi5835hack@gmail.com>
2026-07-09 01:45:26 +0530

    request body handling and user registration schemas with new router endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from app.routers import get_api,path_api,query_api
+from app.routers import get_api,path_api,query_api,body_api
 from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
@@ -13,3 +13,6 @@ app.include_router(path_api.router)
 
 # query parameters with router
 app.include_router(query_api.router)
+
+# Request Body with router
+app.include_router(body_api.router)
