b41c3ed2fb903a39fb1d44ed62912e62bf487eae
Abhishek Kumar <abhi5835hack@gmail.com>
2026-07-09 01:45:26 +0530

    Today's Lesson – GET API

--- app/main.py
+++ app/main.py
@@ -4,5 +4,32 @@ app = FastAPI()
 
 
 @app.get("/")
-async def Home():
-    return {"message": "Hello, I am Start FastApi Learning..."}
+async def home():
+    return {
+        "message": "Welcome to FastAPI 🚀",
+    }
+
+
+@app.get("/about")
+async def about():
+    return {
+        "name": "Abhishek Kumar",
+        "role": "Backend Developer",
+        "age": 28,
+        "address": {
+            "city": "Chapra",
+            "state": "Bihar",
+            "country": "India",
+            "pin-code": "841301"
+        },
+        "skills": ["Python", "FastAPI", "SQL", "Git", "Docker"]
+    }
+
+@app.get("/contact")
+async def contact():
+    return {
+        "email": "abhishek01@gmail.com",
+        "phone": "+91-9876543210",
+    }
+
+        
