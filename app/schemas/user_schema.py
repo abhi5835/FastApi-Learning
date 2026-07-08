644efaba8e9b9851052f8c26f0ff5967148df1e5
Abhishek Kumar <abhi5835hack@gmail.com>
2026-07-09 01:45:27 +0530

    feat: add field validation to User and UserRegistration schemas using Pydantic Field and EmailStr

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,22 +1,43 @@
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 
 class User(BaseModel):
-    name: str
-    email: str
-    age: int
+    name: str = Field(
+        min_length=3,
+        max_length=50,
+        description="User Full Name"
+    )
+    email: str  = EmailStr
+    age: int = Field(
+        ge=18,
+        le=60,
+        description="User Age"
+    )
     # Optional field
-    phone: Optional[str] = None
+    phone: Optional[str] = Field(
+       min_length=8,
+       max_length=20,
+       description="User Phone"
+    )
     # Default value field
     is_active: bool = True
     
 #HomeWork
 
 class UserRegistration(BaseModel):
-    username: str
-    email: str
-    password: str
-    confirm_password: str
+    username: str = Field(min_length=3, max_length=50)
+    email: str = EmailStr
+    password: str = Field(
+        min_length=8,
+        max_length=20,
+        description="Password"
+    )
+    confirm_password: str = Field(
+        min_length=8,
+        max_length=20,
+        description="Confirm Password"
+    )
 
 class UserRegistrationResponse(BaseModel):
     message: str
